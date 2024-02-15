c13ca3f88d96f27c99540845335aa81867470c27
Rahul Kumar <rahul.web404@gmail.com>
2024-02-15 10:38:30 +0530

    task on 15Feb2024

--- NodeJs/tasksFebruary/index.js
+++ NodeJs/tasksFebruary/index.js
@@ -1,16 +1,30 @@
 const express = require('express');
-
+const studentList = require('./studentList');
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Welcome');
 });
 
+app.get('/studentList', (req, res) => {
+  res.send(studentList);
+});
+
+app.get('/studentMERN', (req, res) => {
+  const studentMERN = studentList.filter((el) => el.course === 'MERN');
+  res.send(studentMERN);
+});
+
+app.get('/studentMEAN', (req, res) => {
+  const studentMEAN = studentList.filter((el) => el.course === 'MEAN');
+  res.send(studentMEAN);
+});
+
 app.get('/name', (req, res) => {
   res.send('Rahul');
 });
 app.get('/technology', (req, res) => {
-  res.send('Mern Stack');
+  res.send('MERN Stack');
 });
 app.get('/college', (req, res) => {
   res.send('Ramgarhia Institute of Engineering and technology');
---
+++ NodeJs/tasksFebruary/studentList.js
@@ -0,0 +1,44 @@
+const students = [
+  {
+    id: 1,
+    name: 'Rahul',
+    course: 'MERN',
+  },
+  {
+    id: 2,
+    name: 'Rithik',
+    course: 'MERN',
+  },
+  {
+    id: 3,
+    name: 'Ajay',
+    course: 'MERN',
+  },
+  {
+    id: 4,
+    name: 'Tanveer',
+    course: 'MEAN',
+  },
+  {
+    id: 5,
+    name: 'Gaurav',
+    course: 'MEAN',
+  },
+  {
+    id: 6,
+    name: 'Akashdeep',
+    course: 'MEAN',
+  },
+  {
+    id: 7,
+    name: 'Vishal',
+    course: 'MEAN',
+  },
+  {
+    id: 8,
+    name: 'Rajneesh',
+    course: 'MEAN',
+  },
+];
+
+module.exports = students;
