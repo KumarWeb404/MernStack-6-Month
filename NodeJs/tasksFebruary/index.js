f457348db7f1a9f169c41617fa49aa1cfe5a53a4
Rahul Kumar <rahul.web404@gmail.com>
2024-02-19 13:43:18 +0530

    performed post method

--- NodeJs/tasksFebruary/index.js
+++ NodeJs/tasksFebruary/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const studentList = require('./studentList');
-const filterStudentCourse = require('./filterStudents');
+// const filterStudentCourse = require('./filterStudents');
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(5000, (err) => {
+  err ? console.log('Something went wrong') : console.log('Running');
+});
 
 app.get('/', (req, res) => {
   res.send('Welcome');
@@ -9,16 +15,6 @@ app.get('/', (req, res) => {
 
 app.get('/studentList', (req, res) => {
   res.send(studentList);
-});
-
-app.get('/studentMERN', (req, res) => {
-  const output = filterStudentCourse(studentList, 'MERN');
-  res.send(output);
-});
-
-app.get('/studentMEAN', (req, res) => {
-  const output = filterStudentCourse(studentList, 'MEAN');
-  res.send(output);
 });
 
 app.get('/name', (req, res) => {
@@ -31,19 +27,37 @@ app.get('/college', (req, res) => {
   res.send('Ramgarhia Institute of Engineering and technology');
 });
 
-//using Query string
-app.get('/getStudents', (req, res) => {
-  const { type, name } = req.query;
-  const output = filterStudentCourse(studentList, type);
-  res.send(output);
-});
-
 //using URL params
 app.get('/getStudents/:type/:name', (req, res) => {
   const { type } = req.params;
   const output = filterStudentCourse(studentList, type);
   res.send(output);
 });
-app.listen(5000, (err) => {
-  err ? console.log('Something went wrong') : console.log('Running');
+
+//POST METHOD
+app.post('/addStudent', (req, res) => {
+  const { id, name, course } = req.body;
+  const student = {
+    id: id,
+    name: name,
+    course: course,
+  };
+  studentList.push(student);
+  res.send(student);
 });
+//using Query string
+// app.get('/getStudents', (req, res) => {
+//   const { type, name } = req.query;
+//   const output = filterStudentCourse(studentList, type);
+//   res.send(output);
+// });
+
+// app.get('/studentMERN', (req, res) => {
+//   const output = filterStudentCourse(studentList, 'MERN');
+//   res.send(output);
+// });
+
+// app.get('/studentMEAN', (req, res) => {
+//   const output = filterStudentCourse(studentList, 'MEAN');
+//   res.send(output);
+// });
